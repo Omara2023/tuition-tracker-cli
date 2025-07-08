4f9b7424d904b81b1573111b6218268f4a08ccb6
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-07-08 21:57:33 +0100

    Rename factory function for context manager.

--- db/cm.py
+++ db/cm.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 from db.session import SessionLocal
 
 @contextmanager
-def get_db_session():
+def get_session():
     db = SessionLocal()
     try:
         yield db
