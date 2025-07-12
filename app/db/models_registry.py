e31ea8ceef19e7f4c1fa75a017a2a4d2fdf68d98
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-07-12 22:58:16 +0100

    Implement models_registry.py module for model discovery simplifying.

---
+++ app/db/models_registry.py
@@ -0,0 +1,7 @@
+from app.db.base import Base
+from app.models.parent import Parent
+from app.models.student import Student
+from app.models.rate import Rate
+from app.models.lesson import Lesson
+from app.models.payment import Payment
+from app.models.lesson_payment import LessonPayment
