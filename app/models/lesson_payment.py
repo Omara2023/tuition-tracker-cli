d765cdbf3c99620f1f8609bd54de6aeff3afeb1e
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-08-01 17:46:21 +0100

    Implement sqla 2.0 style models in lessonPayment linking table model.

--- app/models/lesson_payment.py
+++ app/models/lesson_payment.py
@@ -1,13 +1,19 @@
-from sqlalchemy import Column, Integer, ForeignKey, Numeric
-from sqlalchemy.orm import relationship
+from __future__ import annotations
+from sqlalchemy import ForeignKey, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db.base import Base
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models.lesson import Lesson
+    from app.models.payment import Payment
 
 class LessonPayment(Base):
     __tablename__ = "lesson_payments"
 
-    lesson_id = Column(Integer, ForeignKey("lessons.id"), primary_key=True)
-    payment_id = Column(Integer, ForeignKey("payments.id"), primary_key=True)
-    amount_paid = Column(Numeric(10, 2), nullable=False)
+    lesson_id: Mapped[int] = mapped_column(ForeignKey("lessons.id"), primary_key=True)
+    payment_id: Mapped[int] = mapped_column(ForeignKey("payments.id"), primary_key=True)
+    amount_paid: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
 
-    lesson = relationship("Lesson", back_populates="lesson_payments")
-    payment = relationship("Payment", back_populates="lesson_payments")
+    lesson: Mapped[Lesson] = relationship(back_populates="lesson_payments")
+    payment: Mapped[Payment] = relationship(back_populates="lesson_payments")
