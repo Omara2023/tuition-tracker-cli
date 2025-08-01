c954383df10fa6c42c8cb0a754d2273cc5046001
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-08-01 17:33:30 +0100

    Modernise lesson model to 2.0 style.

--- app/models/lesson.py
+++ app/models/lesson.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, Integer, ForeignKey, Enum, Float, UniqueConstraint, Date
-from sqlalchemy.orm import relationship
+from __future__ import annotations
+from datetime import date
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Enum as SAEnum
 from app.db.base import Base
+from typing import TYPE_CHECKING
 import enum
+
+if TYPE_CHECKING:
+    from app.models.rate import Rate
+    from app.models.payment import Payment
+    from app.models.lesson_payment import LessonPayment
 
 class Subjects(enum.Enum):
     MATHEMATICS = "Mathematics"
@@ -15,32 +23,30 @@ class Subjects(enum.Enum):
 class Lesson(Base):
     __tablename__ = "lessons"
 
-    id = Column(Integer, primary_key=True)
-    rate_id = Column(Integer, ForeignKey("rates.id"), nullable=False)
-    subject = Column(Enum(Subjects), nullable=False)
-    duration = Column(Float, nullable=False)
-    date = Column(Date, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    rate_id: Mapped[int] = mapped_column(ForeignKey("rates.id"), nullable=False)
+    subject: Mapped[Subjects] = mapped_column(SAEnum(Subjects), nullable=False)
+    duration: Mapped[float] = mapped_column(nullable=False)
+    date: Mapped[date] = mapped_column(nullable=False)
     
-    rate = relationship("Rate", back_populates="lessons")
-    lesson_payments = relationship("LessonPayment", back_populates="lesson", cascade="all, delete-orphan")
+    rate: Mapped[Rate] = relationship(back_populates="lessons")
+    lesson_payments: Mapped[list[LessonPayment]] = relationship(back_populates="lesson", cascade="all, delete-orphan", lazy="selectin")
     
-    payments = relationship(
-        "Payment",
-        secondary="lesson_payments",
-        viewonly=True,
-        back_populates="lessons",
-    )
+    payments: Mapped[list[Payment]] = relationship(secondary="lesson_payments", back_populates="lessons", viewonly=True, lazy="selectin")
 
     def __repr__(self) -> str:
         return (
-            f"Lesson(id={self.id},rate_id={self.rate_id},"
-            f"subject={self.subject},duration={self.duration}"
-            f"date={self.date})"
+            f"Lesson(id={self.id}, rate_id={self.rate_id},"
+            f"subject={self.subject}, duration={self.duration}, date={self.date})"
         )
     
     def __str__(self) -> str:
-        return f"{self.id} - {self.rate} - {self.subject} - {self.duration}hrs - {self.date}"
-
+        rate = getattr(self, "rate", None)
+        return (
+            f"{self.id} - {rate} - {self.subject} - "
+            f"{self.duration}hrs - {self.date}"
+        )
+    
     @property
     def student(self):
         return self.rate.student
